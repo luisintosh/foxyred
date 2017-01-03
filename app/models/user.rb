f7909ccebbf955c2237a99924f249e72b0ecdb48
Luisintosh <m@luism.net>
2017-01-03 13:34:29 -0600

    user relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,13 @@ class User < ApplicationRecord
   # Active Record Enum for roles
   # http://api.rubyonrails.org/classes/ActiveRecord/Enum.html
   enum role: [:user, :admin]
-  after_initialize :set_default_role, :if => :new_record?
-  after_initialize :set_referral_code, :if => :new_record?
+  after_initialize :configure_user, :if => :new_record?
 
   validates :first_name, :last_name, :address, presence: true
+
+  has_many :withdrawals, dependent: :destroy
+  has_many :links, dependent: :destroy
+  has_one  :balance, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -14,17 +17,18 @@ class User < ApplicationRecord
 
   protected
 
-  def set_default_role
+  def configure_user
     # If user doesn't have role, adds one
     self.role ||= :user
-  end
 
-  def set_referral_code
     # This creates a new code with letters and numbers until doesn't find it in the db
     begin
       code = (0..5).map { rand(36).to_s(36) }.join
     end while User.exists?(referral_code: code)
 
     self.referral_code ||= code
+
+    # Create its account balance
+    self.create_balance(publisher_earnings: 0.0, referral_earnings: 0.0)
   end
 end
