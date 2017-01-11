cd83597f276d2ed503d608c4a4d71b43a8b132a9
Luisintosh <m@luism.net>
2017-01-11 12:26:46 -0600

    ads view

--- app/assets/javascripts/ads.js
+++ app/assets/javascripts/ads.js
@@ -1,2 +1,5 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+//= require theme/js/lib/bootstrap-table/bootstrap-table.js
+//= require theme/js/lib/bootstrap-table/bootstrap-table-export.min.js
+//= require theme/js/lib/bootstrap-table/tableExport.min.js
--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,2 +1,15 @@
 class Ad < ApplicationRecord
+    validates :position, :network, :code, :type, :price, :traffic_source, :countries, :start_date, :end_date, :weight, presence: true
+
+    
+    def self.get_positions
+        [:step1_float_left,
+        :step1_float_right,
+        :step1_banner_center,
+        :step2_frame_url,
+        :step2_float_bottom,
+        :step3_float_left,
+        :step3_float_right,
+        :step3_banner_center]
+    end
 end
