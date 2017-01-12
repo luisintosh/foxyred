853d58c7a107e76d527ab2f86ad79c4c459ad92d
Luisintosh <m@luism.net>
2017-01-11 19:45:09 -0600

    get option by name

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,2 +1,14 @@
 class Option < ApplicationRecord
+
+    def self.get (name)
+        o = Option.find_by(name: name)
+        
+        if o.datatype == 'integer'
+            o = o.value.to_i
+        elsif o.datatype == 'boolean'
+            o = o.value.downcase == 'true'
+        else
+            o = o.value
+        end
+    end
 end
