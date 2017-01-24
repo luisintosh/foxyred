abf29c70b03969a79a3c74103b41c76d4dd06a6f
Luisintosh <m@luism.net>
2017-01-24 15:09:05 -0600

    dashboard view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,16 @@ class DashboardController < ApplicationController
     @views = Link.where(user_id: current_user.id).sum(:hits)
     @earnings = (current_user.balance.publisher_earnings + current_user.balance.referral_earnings)
     @earnings_collected = (current_user.withdrawals.sum(:amount))
+
+    sort = params[:sort] || :id
+    order = params[:order] || :desc
+    limit = params[:limit].to_i || 10
+    limit = (limit <= 100) ? limit : 10
+    @links = current_user.links.all
+                  .search(params[:search])
+                  .order(sort => order)
+                  .offset(params[:offset])
+                  .limit(limit)
   end
 
   def chart_data 
