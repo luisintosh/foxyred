cde66d91454018ec17d8fb0cef94d9fd237a9859
Luisintosh <m@luism.net>
2017-02-11 18:02:17 -0600

    withdraw info fix

--- app/controllers/withdraw_controller.rb
+++ app/controllers/withdraw_controller.rb
@@ -1,5 +1,9 @@
 class WithdrawController < ApplicationController
   def index
     @withdrawals = current_user.withdrawals
+    @total_earnings = current_user.withdrawals.where(status: :paid).last
+    @total_earnings = @total_earnings.amount if @total_earnings
+    @total_earnings ||= 0.0
+    @next_payment = current_user.withdrawals.where(status: :pending).sum(:amount)
   end
 end
