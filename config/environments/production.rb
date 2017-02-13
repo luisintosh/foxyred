b3fa3baa8e7730e7e0c2bdaab6dee8f999b2dcb8
Luisintosh <m@luism.net>
2017-02-13 17:46:40 -0600

    wtf? a comma

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: ENV['SMTP_ADDRESS'],
     port: ENV['SMTP_PORT'],
-    domain: ENV['SMTP_DOMAIN']
+    domain: ENV['SMTP_DOMAIN'],
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
     authentication: :plain,
