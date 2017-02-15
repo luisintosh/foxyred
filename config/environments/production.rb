e0e2d2b9213060f2d0711c2eedcf6644d4f6f617
Luisintosh <m@luism.net>
2017-02-15 10:53:16 -0600

    mailer fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,11 +85,12 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Config mailer
+  config.action_mailer.default_url_options = { :host => ENV['DOKKU_DOMAIN'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: ENV['SMTP_ADDRESS'],
     port: ENV['SMTP_PORT'],
-    domain: ENV['SMTP_DOMAIN'],
+    domain: ENV['DOKKU_DOMAIN'],
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
     authentication: :plain,
