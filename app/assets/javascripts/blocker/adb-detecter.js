b64c8fc5c643caada7688112c1e7cd5c2cbf61f2
Luisintosh <m@luism.net>
2017-02-21 02:38:04 -0600

    adb fix

--- app/assets/javascripts/blocker/adb-detecter.js
+++ app/assets/javascripts/blocker/adb-detecter.js
@@ -10,13 +10,22 @@ $(document).on('turbolinks:load', function() {
     
     // check only on link views
     if ($("meta[name=view-action]").attr('content') == 'link-in') {
+        // random hash
+        var hashID = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 10);
+        var container = [
+            '<div id="',
+            hashID,
+            '" style="display:none;"><div id="amzn_assoc_ad_div_',
+            hashID,
+            '_0"></div>'
+        ];
         // insert ad checker container
-        $(document.body).append('<div id="amzn_assoc_ad_div_xf8_0" style="display:none;"></div>');
+        $(document.body).append(container.join(''));
         // amazon tags
         amzn_assoc_ad_type = "banner";
         amzn_assoc_marketplace = "amazon";
         amzn_assoc_region = "US";
-        amzn_assoc_placement = "xf8";
+        amzn_assoc_placement = hashID;
         amzn_assoc_campaigns = "mp3";
         amzn_assoc_banner_type = "rotating";
         amzn_assoc_p = "41";
@@ -29,19 +38,27 @@ $(document).on('turbolinks:load', function() {
         var defaultWriteFunc = document.write;
         document.write = function(a) {};
 
+        function removeItem() {
+            setTimeout(function() {
+                $('#'+hashID).remove();
+            }, 3000);
+        }
+
         // call to script
         $.getScript('https://z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US')
             .done(function() {
-                //console.log('Adb no detected!');
+                console.log('Adb no detected!');
                 document.write = defaultWriteFunc;
-                $('#amzn_assoc_ad_div_xf8_0').remove();
+                //$('#'+hashID).remove();
+                removeItem();
                 var true_hash = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 10);
                 window.document.cookie = "ABID=" + true_hash + "; path=/";
             })
             .fail(function() {
-                //console.log('Adb detected!');
+                console.log('Adb detected!');
                 document.write = defaultWriteFunc;
-                $('#amzn_assoc_ad_div_xf8_0').remove();
+                //$('#'+hashID).remove();
+                removeItem();
                 var true_hash = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 9);
                 window.document.cookie = "ABID=" + true_hash + "; path=/";
             });
